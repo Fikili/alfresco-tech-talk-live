c9025fc05952b1f63a6581075c4738a7d1df6fa3
Fikili <fikus.b@seznam.cz>
2020-08-07 20:37:03 +0200

    Fixing case sensitivity

--- permission-change/shell-scripts/run_acl_fix.sh
+++ permission-change/shell-scripts/run_acl_fix.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Start ACL fixes";
-input="4_noderef.txt";
-GENERAL_DELAY=50;
+INPUT="3_noderef.txt";
+GENERAL_DELAY=0;
 sh ./generateTicket.sh;
-ticket=$(cat ticket.txt);
+TICKET=$(cat ticket.txt);
 i=0;
 while IFS= read -r line
 do
   echo "$line";
-  $(wget -q "http://localhost:8080/alfresco/s/acl/acl-change-script?noderef=$line&alf_ticket=$ticket");
+  $(wget -q "http://localhost:8080/alfresco/s/acl/acl-change-script?noderef=$line&alf_ticket=$TICKET");
   ((i++));
   if (( $i % 5 == 0 ))
     then
@@ -19,8 +19,8 @@ do
   if (( $i % 200 == 0 ))
     then
       sh ./generateTicket.sh;
-      $ticket=$(cat ticket.txt)
-      echo "A new ticket - "$ticket;
+      TICKET=$(cat ticket.txt);
+      echo "A new ticket - "$TICKET;
   fi
-done < "$input"
+done < "$INPUT"
 echo "Stop ACL fixes";
